1c753f9e40bce259a320f7d11a1f1c1af01a1069
ch-snowman <jack.c.h.0703@gmail.com>
2024-03-06 23:48:03 +0900

    模擬案件１　再提出のコミット

--- js/main.js
+++ js/main.js
@@ -22,3 +22,16 @@ $('#smarttab').smartTab({
   enableUrlHash: false,
   autoAdjustHeight: false,
 });
+
+$(function () {
+  $('a[href^="#"]').click(function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+　　 var speed = 500;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
+});
